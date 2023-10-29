4126f4e9d4b12ed1602969513f3eecd8098f8b2a
qlu1 <qlu1@swarthmore.edu>
2023-10-29 15:19:06 -0400

    import figma

--- app/src/screens/Box/Box.tsx
+++ app/src/screens/Box/Box.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-export const Box = () => {
+export const Box = (): JSX.Element => {
   return (
     <div className="box">
       <img className="rectangle" alt="Rectangle" src="/img/rectangle-5.png" />
