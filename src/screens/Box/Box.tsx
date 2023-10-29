087844de6161d9f563d49565f0653bfff27d7d2c
qlu1 <qlu1@swarthmore.edu>
2023-10-29 15:21:54 -0400

    import figma

---
+++ src/screens/Box/Box.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export const Box = (): JSX.Element => {
+  return (
+    <div className="box">
+      <img className="rectangle" alt="Rectangle" src="/img/rectangle-5.png" />
+    </div>
+  );
+};
