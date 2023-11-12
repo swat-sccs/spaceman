d70b43228a77e6d822a2ff5e01d4b64dab35dac0
George <26337405+Spazzinq@users.noreply.github.com>
2023-11-12 15:38:08 -0500

    Remove ignored files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Raleway } from 'next/font/google'
 import './globals.css'
+import ThemeRegistry from './ThemeRegistry'
 
 const raleway = Raleway({ subsets: ['latin'] })
 
