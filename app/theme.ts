4934bb2a671e8ececd7114280083955238a87bbc
George <26337405+Spazzinq@users.noreply.github.com>
2023-11-12 15:30:03 -0500

    Add MUI theme

---
+++ app/theme.ts
@@ -0,0 +1,51 @@
+import { createTheme } from '@mui/material/styles';
+import { Raleway } from 'next/font/google';
+
+const raleway = Raleway({ subsets: ['latin', 'latin-ext'] });
+
+declare module '@mui/material/styles' {
+  interface PaletteColor {
+    success?: string;
+  }
+}
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    success: {
+      main: '#C2EC7E',
+    },
+    primary: {
+      main: '#31425f',
+    },
+    secondary: {
+      main: '#f46523',
+    },
+    warning: {
+      main: '#f46523',
+    },
+    error: {
+      main: '#86001a',
+    },
+  },
+  shape: {
+    borderRadius: 4,
+  },
+  typography: {
+    allVariants: raleway.style,
+  },
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+    MuiButton: {
+      defaultProps: {
+        variant: 'contained',
+      },
+    },
+  },
+});
+
+export default theme;
